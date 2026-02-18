815431522d785aa232ce571e98e96b9f251e3626
danxguerrero <danxguerrero@yahoo.com>
2026-02-17 22:19:46 -0500

    Added a gradient to the span text in the Hero Section

--- src/pages/HomePage/HeroSection.tsx
+++ src/pages/HomePage/HeroSection.tsx
@@ -1,14 +1,30 @@
 const HeroSection = () => {
-    return <div className="bg-slate-900 text-slate-50 flex flex-col items-center justify-center pt-12 px-2">
-        <div className="w-full md:max-w-5xl flex flex-col items-center ">
-            <div className="text-center text-5xl md:text-7xl mb-6">Building <span className="text-cyan-500">scalable</span> software solutions for the modern web.</div>
-            <div className="text-center text-slate-300 max-w-2xl text-sm sm:text-lg">I am a software engineer passionate about crafting high-performance interacitve digital experiences and solving complex problems with clean code.</div>
-            <div className="flex justify-center m-12 text-lg w-full">
-                <button className="bg-cyan-500 hover:border hover:border-slate-50 text-slate-50 rounded-lg px-3 py-2 mr-3">View My Work</button>
-                <button className="border border-slate-50 hover:border-cyan-500 hover:text-cyan-500 px-3 py-2 rounded-lg">Get in Touch</button>
-            </div>
+  return (
+    <div className="flex flex-col items-center justify-center bg-slate-900 px-2 pt-12 text-slate-50">
+      <div className="flex w-full flex-col items-center md:max-w-5xl">
+        <div className="mb-6 text-center text-5xl md:text-7xl">
+          Building{" "}
+          <span className="bg-gradient-to-r from-cyan-500 to-emerald-400 bg-clip-text text-transparent">
+            scalable
+          </span>{" "}
+          software solutions for the modern web.
         </div>
+        <div className="max-w-2xl text-center text-sm text-slate-300 sm:text-lg">
+          I am a software engineer passionate about crafting high-performance
+          interacitve digital experiences and solving complex problems with
+          clean code.
+        </div>
+        <div className="m-12 flex w-full justify-center text-lg">
+          <button className="mr-3 rounded-lg bg-cyan-500 px-3 py-2 text-slate-50 hover:border hover:border-slate-50">
+            View My Work
+          </button>
+          <button className="rounded-lg border border-slate-50 px-3 py-2 hover:border-cyan-500 hover:text-cyan-500">
+            Get in Touch
+          </button>
+        </div>
+      </div>
     </div>
-}
+  );
+};
 
 export default HeroSection;
