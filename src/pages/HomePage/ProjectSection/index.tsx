0402eb45a93c2a90eb5e63fbfe109746ca95d6a3
danxguerrero <danxguerrero@yahoo.com>
2026-02-20 21:11:18 -0500

    Create a ProjectItem component for each project

--- src/pages/HomePage/ProjectSection/index.tsx
+++ src/pages/HomePage/ProjectSection/index.tsx
@@ -1,14 +1,24 @@
+import { projectData } from "./data/projects";
+import ProjectItem from "./ProjectItem";
+
 const ProjectSection = () => {
+  const projectItems = projectData.map((project, idx) => (
+    <ProjectItem project={project} key={idx} />
+  ));
   return (
-    <div className="flex justify-center m-12">
-      <div className="flex flex-col w-full max-w-5xl">
+    <div className="m-12 flex justify-center">
+      <div className="flex w-full max-w-5xl flex-col">
         <div className="flex flex-col">
           <p className="text-cyan-500">Featured Projects</p>
-          <div className="text-slate-50 text-4xl my-3">Recent Projects</div>
+          <div className="my-3 text-4xl text-slate-50">Recent Projects</div>
           <div className="flex sm:justify-end">
-            <button className="text-cyan-500 hover:text-cyan-400">Browse all projects <i className="fa-solid fa-arrow-right ml-1"></i></button>
+            <button className="text-cyan-500 hover:text-cyan-400">
+              Browse all projects{" "}
+              <i className="fa-solid fa-arrow-right ml-1"></i>
+            </button>
           </div>
         </div>
+        <div className="flex justify-between mt-12">{projectItems}</div>
       </div>
     </div>
   );
