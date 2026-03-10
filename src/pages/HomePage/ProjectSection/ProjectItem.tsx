a7f28672186e320378d60d22bacbbbe0560b03e0
danxguerrero <danxguerrero@yahoo.com>
2026-03-10 15:39:49 -0400

    Implement ProjectItems for featured projects

--- src/pages/HomePage/ProjectSection/ProjectItem.tsx
+++ src/pages/HomePage/ProjectSection/ProjectItem.tsx
@@ -6,9 +6,20 @@ type ProjectItemProps = {
 
 const ProjectItem = ({project}: ProjectItemProps) => {
 
+    // TODO: Implement stackItems
+    // const stackItems = project.stack.map((tech, idx) => <div key={idx}>{tech}</div>)
+
     return (
-        <div className=" bg-slate-50">
-            {project.name}
+        <div className="m-3 w-[300px] rounded-lg">
+            <img src={project.imgUrl} alt={`${project.name} logo`} className="h-[275px] w-full rounded-t-lg" />
+            <div className="bg-slate-800 text-slate-50 rounded-b-lg px-3 py-2">
+                <div className="mb-3 text-xl">{project.name}</div>
+                <div className="text-wrap text-sm md:text-base">{project.description}</div>
+                <div className="flex mt-3">
+                    {project.deployedUrl && <a className="mr-2" href={project.deployedUrl} target="_blank"><i className="fa-solid fa-link text-lg"></i></a>}
+                    <a href={project.githubUrl} target="_blank"><i className="fa-brands fa-github"></i></a>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/HomePage/ProjectSection/data/projects.ts
+++ src/pages/HomePage/ProjectSection/data/projects.ts
@@ -24,7 +24,7 @@ export const projectData: projectType[] = [
     deployedUrl: "https://mocking-bird-rho.vercel.app/",
     githubUrl: "https://github.com/danxguerrero/MockingBird",
     group: "solo",
-    imgUrl: "https://imgur.com/a/BAFbvVQ",
+    imgUrl: "https://imgur.com/NY7eHfc.png",
   },
   {
     name: "MockingBird CLI",
@@ -34,7 +34,7 @@ export const projectData: projectType[] = [
     deployedUrl: "",
     githubUrl: "https://github.com/danxguerrero/mockingbird-cli",
     group: "solo",
-    imgUrl: "https://imgur.com/a/BAFbvVQ",
+    imgUrl: "https://imgur.com/NY7eHfc.png",
   },
   {
     name: "Project Hermes",
@@ -44,6 +44,6 @@ export const projectData: projectType[] = [
     deployedUrl: "https://atlaus-project-hermes.netlify.app/",
     githubUrl: "https://github.com/ATLAUS/Project-Hermes-React",
     group: "ATLAUS",
-    imgUrl: "https://imgur.com/a/project-hermes-logo-veoi5mF"
+    imgUrl: "https://imgur.com/En85emj.png"
   },
 ];
