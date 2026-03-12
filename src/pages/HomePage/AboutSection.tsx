5ea23f409c43f161a5c8da5f39435047d8376273
danxguerrero <danxguerrero@yahoo.com>
2026-03-12 15:12:28 -0400

    Create the AboutSection component with initial styles and text

---
+++ src/pages/HomePage/AboutSection.tsx
@@ -0,0 +1,21 @@
+const AboutSection = () => {
+  return (
+    <div className="m-12 flex justify-center">
+      <div className="flex w-full max-w-5xl flex-col">
+        <p className="text-4xl text-slate-50">About</p>
+        <p className="text-slate-50">
+          I'm a self-taught developer who loves the intersection of logic and
+          design. My journey started with automating simple tasks in Python,
+          which quickly evolved into building full-scale web applications as a Software Engineer Apprentice at Google.
+          <br />
+          <br />
+          I thrive in collaborative enviornments and enjoy breaking down
+          complex problems into manageable components. When Im not coding,
+          you'll find me in the gym or tinkering with mechanical keyboards.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
